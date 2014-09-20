3e856ca4da9621c112f4cca85155ec125aed9941
Dean <Dean.leitersdorf@gmail.com>
2014-09-19 21:20:16 -0700

    d

--- requestHandling.js
+++ requestHandling.js
@@ -2,6 +2,8 @@ var querystring = require("querystring");
 
 var nodemailer = require('nodemailer');
 
+
+
 // create reusable transporter object using SMTP transport
 var transporter = nodemailer.createTransport({
     service: 'Gmail',

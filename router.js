d048075db5188171685898b1fdbc799a475b7efd
Dean <Dean.leitersdorf@gmail.com>
2014-11-02 11:54:35 -0800

    M

--- router.js
+++ router.js
@@ -4,7 +4,7 @@
 
 function route(handle, pathname, response, postData) {
 	pathname = pathname.toLowerCase();
-	console.log("About to route a request for " + pathname);
+	console.log("About to route a request for " + pathname + " , with data: " + postData);
 	
 	if (typeof handle[pathname] === 'function') {
 		handle[pathname](response, postData);

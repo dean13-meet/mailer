b036c5645bdd0f50bf72c9e80d6507b8a8b0ddf7
Dean <Dean.leitersdorf@gmail.com>
2014-11-18 09:08:04 -0800

    M

--- resturantHandlers.js
+++ resturantHandlers.js
@@ -553,7 +553,7 @@ function getImageByID(response2, postdata, image)
 		{
 		console.log("Sending image: " + image);
 		console.log("Sending image json: " + JSON.stringify(image));
-		response2.write('Content-Length:'+data.length+'\r\n');
+		response2.write('Content-Length:'+image.data.length+'\r\n');
 		response2.write('Content-Type:' + image.Content_Type + '\r\n');
 		response2.end(image.data);
 		}

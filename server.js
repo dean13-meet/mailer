f9c8c6f5dcb1a12296aada814229cd1bf3652594
Dean <Dean.leitersdorf@gmail.com>
2014-11-25 15:38:10 -0800

    M

--- server.js
+++ server.js
@@ -39,7 +39,13 @@ function start(route, handle) {
 			    console.log('user disconnected');
 			  });
 		});
-	
+	server.on('connection', function(socket){
+		  console.log('a user connected');
+		  
+		  socket.on('disconnect', function(){
+			    console.log('user disconnected');
+			  });
+		});
 }
 
 exports.start = start;

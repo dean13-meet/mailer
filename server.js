5676cfc958df81f5eb3a432905f8e54f92235809
Dean <Dean.leitersdorf@gmail.com>
2014-11-25 09:19:45 -0800

    M

--- server.js
+++ server.js
@@ -28,6 +28,17 @@ function start(route, handle) {
 	}
 	http.createServer(onRequest).listen(process.env.PORT || 5000);
 	console.log("Server has started.");
+	
+
+	var io = require('socket.io')(http);
+	io.on('connection', function(socket){
+		  console.log('a user connected');
+		  
+		  socket.on('disconnect', function(){
+			    console.log('user disconnected');
+			  });
+		});
+	
 }
 
 exports.start = start;

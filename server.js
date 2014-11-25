6af999ddd06aa7ffac51fa3e3e634ad607651e94
Dean <Dean.leitersdorf@gmail.com>
2014-11-25 15:20:48 -0800

    M

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function start(route, handle) {
 	console.log("Server has started.");
 	
 
-	var io = require('socket.io').listen(3000);
+	var io = require('socket.io')(80);
 	io.on('connection', function(socket){
 		  console.log('a user connected');
 		  

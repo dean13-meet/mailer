1e4b2ed0dd9e16616fffca5b4a0dbb06c05b1222
Dean <Dean.leitersdorf@gmail.com>
2014-11-30 11:43:39 -0800

    M

--- resturantHandlers.js
+++ resturantHandlers.js
@@ -1152,7 +1152,13 @@ function printTrackers(response, postdata, trackers)
 	//explanation for trackers.hasOwnProperty... : http://stackoverflow.com/questions/558981/iterating-through-list-of-keys-for-associative-array-in-json
 	for (var key in trackers) {
 		  if (trackers.hasOwnProperty(key)) {
-		    response.write(key + ":" + trackers[key].id + ":" + trackers[key].isOn);
+		    response.write(key + ":");
+		    for(i = 0; i < trackers[key].length; i++)
+		    	{
+		    	client = trackers[key][i];
+		    	response.write(client.id + ":" + client.isOn);
+		    	}
+		    response.write("");//blank line
 		  }
 		}
 	response.end();

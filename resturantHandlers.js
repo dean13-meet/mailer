703b4a5fcd98497b39d81ee838110049c18e884a
Dean <Dean.leitersdorf@gmail.com>
2014-12-11 09:01:56 -0800

    M

--- resturantHandlers.js
+++ resturantHandlers.js
@@ -913,7 +913,7 @@ function getMessagesFromChatObject(socket, postdata, trackers, user, chatObject)
 	messagesToReturn = messagesToReturn.slice(indexToReturn, messagesToReturn.length);
 	
 	if(socket)
-		socket.send(JSON.stringify({"answerForGetMessagesFromChatObject":chatObject.id,"messages": messagesToReturn, "shouldTimeSkip":shouldTimeSkip}));
+		socket.send(JSON.stringify({"answerForGetMessagesFromChatObject":chatObject.id,"messages": messagesToReturn, "shouldTimeSkip":shouldTimeSkip, "messageIndex":chatObject.messages.length}));
 	else
 		console.log(JSON.stringify({"messages": messagesToReturn, "shouldTimeSkip":shouldTimeSkip}));
 	console.log("sent messages");

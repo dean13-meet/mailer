57699bfd2534b879e4188e60503b2c45d6c8bc07
Dean <Dean.leitersdorf@gmail.com>
2014-12-13 22:02:28 -0800

    M

--- resturantHandlers.js
+++ resturantHandlers.js
@@ -611,7 +611,7 @@ function postMessage(socket, postdata, trackers, user, chatObject)
 	if( typeof chatObject.messages != typeof [])chatObject.messages = [];
 	chatObject.messages.push(message);
 	chatObject.timestamp = message.timestamp;
-	saveObject(chatObject.id, chatObject, [chatObject.id+"/"+"messages"]);
+	saveObject(chatObject.id, chatObject, [chatObject.id+"/"+"messages"], trackers);
 }
 exports.postMessage = postMessage;
 

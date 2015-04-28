dcffb42f59148846091c9f4fee919b5d4a331755
Dean <Dean.leitersdorf@gmail.com>
2015-04-27 21:59:12 -0700

    M

--- main.js
+++ main.js
@@ -71,11 +71,11 @@ handle["/resturant/addmessage"] = requestHandlers.resturantApp.addMessage;
 
 server.start(router.route, handle);
 
-
+/*
 json1 = {"userKnownIdentifier":"k01tEWfxpWaXGxJ4LctUsDENqVmUtG", "mode":1, "lat":42.36064, "long":-71.08974}
 requestHandlers.itrack.sendFenceMessage(0, json1);
 
-/*
+
 json2 = {"name":"deanL", "auth":0912}
 requestHandlers.itrack.verifyAuthForUserName("", json2);
 

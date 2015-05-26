17291050bc14adb4e6b852f2148dc78e7ac5e1d7
Dean <Dean.leitersdorf@gmail.com>
2015-05-26 08:17:25 -0700

    M

--- itrackHandlers.js
+++ itrackHandlers.js
@@ -548,7 +548,9 @@ function sendFenceMessage(response, postdata, trackers)
 			sendMessageToNumbers(numbers, message, geofence.owner);
 			
 			
-			response.end(JSON.stringify({showMessage : "Sending message" + (numbers.length>1?"s":"") + " for " + geofence.address.split("\n")[0]}));
+			response.end(JSON.stringify({showMessage : "Sending message" + (numbers.length>1?"s":"") + " for " + geofence.address.split("\n")[0],
+			geofence:geofence
+			}));
 			// response.end("Try?");
 			currentTime = Math.floor(new Date() / 1000);
 			mode ? geofence.arrivalsSent.push(currentTime):geofence.leavesSent.push(currentTime);
